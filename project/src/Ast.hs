7aa65abae90aef79b1f6d0d559d3b9df124e3a27
cnhexin <bupthexin@hotmail.com>
2018-12-06 17:40:17 -0500

    last job

--- project/src/Ast.hs
+++ project/src/Ast.hs
@@ -4,6 +4,22 @@ data Program -- TODO: your ast here
 
 -- TODO: Ast should have at least Eq and Show instances
 
+data Stmts = Assign String Expr |
+             While Expr Stmts |
+             Block [Stmts] |
+             If Expr Stmts |
+             Else Stmts 
+             deriving Eq
+       
 
+data Expr = ValInt Integer |
+            Plus Expr Expr | Minus Expr Expr | Mult Expr Expr | Div Expr Expr
+            deriving Eq
+eval :: Expr -> Integer
+eval (ValInt x) = x
+eval (Plus x y) = eval x + eval y 
+instance Show Expr where
+  show (ValInt x) = show x
+  show (Plus x y) = show (eval x) ++ "+" ++ show (eval y)
 prettyShow :: Program -> String
 prettyShow = undefined
