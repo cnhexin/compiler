71421d7bf2c995416712a4b742fb1a66957419f4
Ruihong Zhu <raymondzhu1996@gmail.com>
2018-12-06 18:54:25 -0500

    update1

--- project/src/Ast.hs
+++ project/src/Ast.hs
@@ -1,6 +1,6 @@
 module Ast where
 
-data Program -- TODO: your ast here
+type Program = [Stmts] -- TODO: your ast here
 
 -- TODO: Ast should have at least Eq and Show instances
 
@@ -13,13 +13,25 @@ data Stmts = Assign String Expr |
        
 
 data Expr = ValInt Integer |
-            Plus Expr Expr | Minus Expr Expr | Mult Expr Expr | Div Expr Expr
+            Plus Expr Expr | Minus Expr Expr | Times Expr Expr | Div Expr Expr
             deriving Eq
-eval :: Expr -> Integer
-eval (ValInt x) = x
-eval (Plus x y) = eval x + eval y 
-instance Show Expr where
-  show (ValInt x) = show x
-  show (Plus x y) = show (eval x) ++ "+" ++ show (eval y)
-prettyShow :: Program -> String
-prettyShow = undefined
+
+--eval :: Expr -> Integer
+--eval (ValInt x) = x
+--eval (Plus x y) = eval x + eval y 
+
+--instance Show Expr where
+  --show (ValInt x) = show x
+  --show (Plus x y) = show (eval x) ++ "+" ++ show (eval y)
+prettyShowS :: Stmts -> String
+prettyShowS (Assign s a) = "Assign " ++ s ++ " " ++ (prettyShowE a)
+prettyShowS (While a s) = "While " ++ (prettyShowE a) ++ " " ++ (prettyShowS s)
+ 
+prettyShowE :: Expr -> String
+prettyShowE (ValInt i) = if i < 0
+                        then  "(" ++ show i ++ ")"
+                        else show i
+prettyShowE (Plus x y) = (prettyShowE x) ++ " + " ++ (prettyShowE y)
+prettyShowE (Minus x y) = (prettyShowE x) ++ " - " ++ (prettyShowE y)
+prettyShowE (Times x y) = (prettyShowE x) ++ " * " ++ (prettyShowE y)
+prettyShowE (Div x y) = (prettyShowE x) ++ " / " ++ (prettyShowE y)
