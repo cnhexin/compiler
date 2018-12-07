f2c68e589794fc6a0a21955407ee3fa5934b8c4e
Ruihong Zhu <raymondzhu1996@gmail.com>
2018-12-06 19:39:35 -0500

    update2

--- project/src/CParser.hs
+++ project/src/CParser.hs
@@ -5,3 +5,27 @@ import ParserMonad
 
 parser :: Parser Program
 parser = undefined
+
+ints :: Parser Expr
+ints = do i <- token $ intParser
+          return $ ValInt i
+		  
+addSubExpr :: Parser Expr
+addSubExpr = withInfix multDivExpr [("+",Plus),("-", Minus)]
+
+multDivExpr :: Parser Expr
+multDivExpr = withInfix notExp [("*",Times),("/",Div)]
+
+notExp :: Parser Expr
+notExp = undefined
+
+atoms :: Parser Expr
+atoms = ints 
+
+parens :: Parser Program
+parens = do token (literal "(")
+            res <- parser
+            token (literal ")")
+            return res	 
+
+
